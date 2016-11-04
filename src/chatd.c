88423c55aa1e2a324d9688e98b9ca9eee0bfdcb8
Stinni <kristinnf13@ru.is>
2016-11-04 01:04:14 +0000

    Initial commit

---
+++ src/chatd.c
@@ -0,0 +1,114 @@
+/* A TCP echo server with timeouts.
+ *
+ * Note that you will not need to use select and the timeout for a
+ * tftp server. However, select is also useful if you want to receive
+ * from multiple sockets at the same time. Read the documentation for
+ * select on how to do this (Hint: Iterate with FD_ISSET()).
+ */
+
+// Constants:
+#define MAX_MESSAGE_LENGTH 1024
+
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Secure socket layer headers */
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#include <glib.h>
+#include <glib/gprintf.h>
+
+/* This can be used to build instances of GTree that index on
+   the address of a connection. */
+int sockaddr_in_cmp(const void *addr1, const void *addr2)
+{
+    const struct sockaddr_in *_addr1 = addr1;
+    const struct sockaddr_in *_addr2 = addr2;
+
+    /* If either of the pointers is NULL or the addresses
+       belong to different families, we abort. */
+    g_assert((_addr1 == NULL) || (_addr2 == NULL) ||
+             (_addr1->sin_family != _addr2->sin_family));
+
+    if (_addr1->sin_addr.s_addr < _addr2->sin_addr.s_addr) {
+        return -1;
+    } else if (_addr1->sin_addr.s_addr > _addr2->sin_addr.s_addr) {
+        return 1;
+    } else if (_addr1->sin_port < _addr2->sin_port) {
+        return -1;
+    } else if (_addr1->sin_port > _addr2->sin_port) {
+        return 1;
+    }
+    return 0;
+}
+
+/* This can be used to build instances of GTree that index on
+   the file descriptor of a connection. */
+gint fd_cmp(gconstpointer fd1,  gconstpointer fd2, gpointer G_GNUC_UNUSED data)
+{
+     return GPOINTER_TO_INT(fd1) - GPOINTER_TO_INT(fd2);
+}
+
+int main(int argc, char **argv)
+{
+    int sockfd;
+    struct sockaddr_in server, client;
+
+    if (argc != 2) {
+         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+         exit(EXIT_FAILURE);
+    }
+
+    const int server_port = strtol(argv[1], NULL, 10);
+
+    gchar message[MAX_MESSAGE_LENGTH];
+
+    /* Create and bind a TCP socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* Network functions need arguments in network byte order instead of
+       host byte order. The macros htonl, htons convert the values. */
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = server_port;
+    bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+
+    /* Before the server can accept messages, it has to listen to the
+       welcome port. A backlog of six connections is allowed. */
+    listen(sockfd, 6);
+
+     /* Initialize OpenSSL */
+    //Used in chat.c
+    /*static int server_fd;
+    static SSL *server_ssl;*/
+    SSL_library_init(); /* load encryption & hash algorithms for SSL */
+    SSL_load_error_strings(); /* load the error strings for good error reporting */
+
+
+
+     /* Receive and handle messages. */
+    for(;;) {
+        //Accepting a TCP connection, connfd is a handle dedicated to this connection.
+        socklen_t len = (socklen_t) sizeof(client);
+        int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
+
+        /* Receive from connfd, not sockfd. */
+        ssize_t n = recv(connfd, message, sizeof(message) - 1, 0);
+        message[n] = '\0';
+
+        for(unsigned int i = 0; i < n; i++) g_printf("%hhx ", message[i]);
+        g_printf("\n");
+    }
+
+    exit(EXIT_SUCCESS);
+}
